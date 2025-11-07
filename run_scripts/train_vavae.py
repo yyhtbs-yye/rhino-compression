67dd7db92bd0f351f20e47c79f9a8fcaf1da7947
yyhtbs-yye <yyhtbs1@gmail.com>
2025-11-07 12:26:30 +0000

    add dispersive sdvae

--- run_scripts/train_vavae.py
+++ run_scripts/train_vavae.py
@@ -3,7 +3,7 @@ from rhtrain.rhino_train import main
 
 args_dict = {
     'config': 'configs/train_vavae_256_high_kl.yaml',
-    'resume_from': None,
+    'resume_from': 'work_dirs/vavae_ffhq_256_high_kl/run_1/last.pt',
 }
 
 args = types.SimpleNamespace(**args_dict)
